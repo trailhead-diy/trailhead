9ceb995019bb20a38fd7b78842170aa42336c766
esteban-url <14810250+esteban-url@users.noreply.github.com>
2025-07-28 13:28:53 -0600

    chore: remove web-ui

--- packages/web-ui/src/components/divider.tsx
+++
@@ -1,9 +0,0 @@
-import { type ComponentProps } from 'react'
-import { cn } from './utils/cn'
-import { CatalystDivider } from './lib/catalyst-divider'
-
-export type DividerProps = ComponentProps<typeof CatalystDivider>
-
-export const Divider = ({ className, ...props }: DividerProps) => (
-  <CatalystDivider className={cn(className)} {...props} />
-)
