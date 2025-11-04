9411bca53a54907f690b0893a9311fe0cbb52055
Esteban Rojas <esteban-url@users.noreply.github.com>
2025-11-04 13:01:02 -0600

    chore: remove build artifacts and add .docusaurus to .gitignore
    Remove accidentally committed Docusaurus build artifacts that were
    included in previous commit. These files should never be tracked.
    
    - Remove apps/docs/.docusaurus/ directory and contents
    - Add .docusaurus/ to .gitignore

--- apps/docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/e/gh/trailhead/apps/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css'),
-  require('/Users/e/gh/trailhead/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/e/gh/trailhead/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.9_acorn@8.15.0_lightningcss@1.30.1_re_ec739b55865e438b3ab31f68f40a3516/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/e/gh/trailhead/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.9_acorn@8.15.0_lightningcss@1.30.1_re_ec739b55865e438b3ab31f68f40a3516/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/e/gh/trailhead/apps/docs/src/css/custom.css'),
-]
